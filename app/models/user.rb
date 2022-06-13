76f4bac7a7973277aae2be8b5b33e1e7550496d0
akira-no <zzzzzgm4021@gmail.com>
2022-06-13 21:11:45 +0900

    モデルテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname,       presence: true
-  validates :last_name,      presence: true
-  validates :first_name,     presence: true
-  validates :name_sei,       presence: true
-  validates :name_mei,       presence: true
+  validates :last_name,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name,     presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :name_sei,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :name_mei,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birth_date,     presence: true
 end
