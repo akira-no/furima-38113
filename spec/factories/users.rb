241cb4efd56ade8b543b636838570924d72fdca1
akira-no <zzzzzgm4021@gmail.com>
2022-06-13 21:15:14 +0900

    Rubocop済

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,17 @@
 FactoryBot.define do
-
   factory :user do
-
     transient do
-      person {Gimei.name}
+      person { Gimei.name }
     end
 
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {person::last.kanji}
-    first_name            {person::first.kanji}
-    name_sei              {person::last.katakana}
-    name_mei              {person::first.katakana}
-    birth_date            {Faker::Date.backward}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    name_sei              { person.last.katakana }
+    name_mei              { person.first.katakana }
+    birth_date            { Faker::Date.backward }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
-    
+
     context '新規登録できないとき' do
       it 'nicknameが空' do
         @user.nickname = ''
@@ -29,13 +29,13 @@ RSpec.describe User, type: :model do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
       it 'emailに@が含まれていない' do
         @user.email = 'testmail'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
 
       it 'passwordが空' do
@@ -47,19 +47,19 @@ RSpec.describe User, type: :model do
       it 'passwordが6文字未満' do
         @user.password = '12345'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
       it 'passwordが半角英数字混合ではない(数字のみ)' do
         @user.password = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
       it 'passwordが半角英数字混合ではない(アルファベットのみ)' do
         @user.password = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
       it 'passwordと確認passwordが一致しない' do
@@ -84,13 +84,13 @@ RSpec.describe User, type: :model do
       it 'last_nameが全角漢字・ひらがな・カタカナ以外' do
         @user.last_name = '123'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid")
+        expect(@user.errors.full_messages).to include('Last name is invalid')
       end
 
       it 'first_nameが全角漢字・ひらがな・カタカナ以外' do
         @user.first_name = '123'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
 
       it 'name_seiが空' do
@@ -108,14 +108,15 @@ RSpec.describe User, type: :model do
       it 'name_seiがカタカナ以外' do
         @user.name_sei = 'あいう'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Name sei is invalid")
+        expect(@user.errors.full_messages).to include('Name sei is invalid')
       end
 
       it 'name_meiがカタカナ以外' do
         @user.name_mei = 'あいう'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Name mei is invalid")
+        expect(@user.errors.full_messages).to include('Name mei is invalid')
       end
+      
       it 'birth_dateが空' do
         @user.birth_date = ''
         @user.valid?
@@ -124,4 +125,3 @@ RSpec.describe User, type: :model do
     end
   end
 end
-
