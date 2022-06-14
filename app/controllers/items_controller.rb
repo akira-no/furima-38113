9afbf7605187c920cf336e7ccf7d255e7c786990
akira-no <zzzzzgm4021@gmail.com>
2022-06-14 16:24:30 +0900

    item.model validation 追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,15 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    
   end
 
   def new
     @item = Item.new
   end
 
+  private
+  
+  def item_params
+    params.require(:item).permit(:image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+  has_one_attached :image
   belongs_to :user
-end
+
+  validates :product,                presence: true, length: { maximum: 8 }
+  validates :explanation,            presence: true, length: { maximum: 1000 }
+  validates :condition_id,           presence: true, numericality: { other_than: 1 }
+  validates :category_id,            presence: true, numericality: { other_than: 1 }
+  validates :delivery_charge_id,     presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id,          presence: true, numericality: { other_than: 1 }
+  validates :delivery_day_id,        presence: true, numericality: { other_than: 1 }
+  validates :price,                  presence: true, length: { minimum: 300 , maximum: 9999999}
