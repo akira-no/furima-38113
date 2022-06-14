8140f41c01c0bc1fd5ea5d989a7be1c3af5f4630
akira-no <zzzzzgm4021@gmail.com>
2022-06-14 23:20:48 +0900

    テスト用画像作成 & テスト項目洗い出し

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :item do
-    image              {Faker::Lorem.sentence}
-    product            {Faker::Lorem.sentence(max_length: 40)}
-    explanation        {Faker::Lorem.sentence(max_length: 1000)}
+    product            {Faker::Lorem.sentence}
+    explanation        {Faker::Lorem.sentence}
     condition_id       {Faker::Number.between(2, 7)}
     category_id        {Faker::Number.between(2, 11)}
     delivery_charge_id {Faker::Number.between(2, 3)}
     prefecture_id      {Faker::Number.between(2, 48)}
     delivery_day_id    {Faker::Number.between(2, 4)}
     price              {Faker::Number.between(300, 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/testimage.png'), filename: 'testimage.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品できるとき' do
+      it '9項目全て正しく記述があれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+    
+    context '商品出品できないとき' do
+      it '商品画像入力がない' do
+      end
+      it '商品名入力がない' do
+      end
+      it '商品名が40文字以上' do
+      end
+      it '商品説明入力がない' do
+      end
+      it '商品説明が1000文字以上' do
+      end
+      it 'カテゴリー入力がない' do
+      end
+      it 'カテゴリー選択が --- ' do
+      end
+      it '商品状態入力がない' do
+      end
+      it '商品状態選択が --- ' do
+      end
+      it '配送料入力がない' do
+      end
+      it '配送料選択が --- ' do
+      end
+      it '発送元入力がない' do
+      end
+      it '発送元選択が --- ' do
+      end
+      it '発送日数入力がない' do
+      end
+      it '発送日数選択が --- ' do
+      end
+      it '価格入力がない' do
+      end
+      it '価格が300未満' do
+      end
+      it '価格が9999999超過' do
+      end
+      it '価格が全角数字入力' do
+      end
+      it '価格が半角数字+全角数字入力' do
+      end
+    end
+  end
 end
