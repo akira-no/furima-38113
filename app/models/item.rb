52b032f8c394f46c9ea47a714d37a8691242fee3
akira-no <zzzzzgm4021@gmail.com>
2022-06-14 19:14:40 +0900

    active_hash testOK

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
 
   validates :product,                presence: true, length: { maximum: 8 }
   validates :explanation,            presence: true, length: { maximum: 1000 }
-  validates :condition_id,           presence: true, numericality: { other_than: 1 }
+  validates :condition_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :category_id,            presence: true, numericality: { other_than: 1 }
   validates :delivery_charge_id,     presence: true, numericality: { other_than: 1 }
   validates :prefecture_id,          presence: true, numericality: { other_than: 1 }
