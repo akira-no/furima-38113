8158b73ea00286042f0ecaa39db16fad52ae9fe3
akira-no <zzzzzgm4021@gmail.com>
2022-06-14 19:49:25 +0900

    active_hash 出品全項目に適用

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :condition
+  belongs_to :category
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
   validates :product,                presence: true, length: { maximum: 8 }
   validates :explanation,            presence: true, length: { maximum: 1000 }
   validates :condition_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :category_id,            presence: true, numericality: { other_than: 1 }
-  validates :delivery_charge_id,     presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id,          presence: true, numericality: { other_than: 1 }
-  validates :delivery_day_id,        presence: true, numericality: { other_than: 1 }
+  validates :category_id,            presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,          presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,                  presence: true, length: { minimum: 300 , maximum: 9999999}
