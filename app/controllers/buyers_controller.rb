5dc7cae4713226e58749b726af8fbc1dadc3bab8
akira-no <zzzzzgm4021@gmail.com>
2022-06-17 11:18:10 +0900

    購入view追加, controller,models 購入処理追加(仮)

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,18 @@
 class BuyersController < ApplicationController
+  def index
+    @buyer_address = BuyerAddress.new
+  end
+
+  def create
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer_address).permit(:post_code, :street_name, :house_number, :prefecture_id, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -3,12 +3,18 @@ class BuyerAddress
   attr_accessor :user_id, :item_id, :post_code, :street_name, :house_number, :prefecture_id, :building_name, :phone_number, :buyer_id
 
   with_options presence: true do
-    validates :user_id,
-    validates :item_id,
+    validates :user_id
+    validates :item_id
     validates :post_code,　　　　　　3桁ハイフン4桁の半角文字列のみ保存可能なこと（良い例：123-4567　良くない例：1234567）。
-    validates :street_name,
-    validates :house_number,
-    validates :prefecture_id,
+    validates :street_name
+    validates :house_number
+    validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }
     validates :phone_number,　　　　10桁以上11桁以内の半角数値のみ保存可能なこと（良い例：09012345678　良くない例：090-1234-5678
-    validates :buyer_id,
+    validates :buyer_id
+  end
+
+  def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, street_name: street_name, house_number: house_number, prefecture_id: prefecture_id, building_name: building_name, phone_number: phone_number, buyer_id: buyer.id)
+  end
 end
