3cbf2f3c295e5ce100d92cb51a950df11dd0646c
akira-no <zzzzzgm4021@gmail.com>
2022-06-17 12:07:21 +0900

    購入画面への遷移追加

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -5,11 +5,11 @@ class BuyerAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code,　　　　　　3桁ハイフン4桁の半角文字列のみ保存可能なこと（良い例：123-4567　良くない例：1234567）。
+    # validates :post_code,　　　　　　3桁ハイフン4桁の半角文字列のみ保存可能なこと（良い例：123-4567　良くない例：1234567）。
     validates :street_name
     validates :house_number
     validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }
-    validates :phone_number,　　　　10桁以上11桁以内の半角数値のみ保存可能なこと（良い例：09012345678　良くない例：090-1234-5678
+    # validates :phone_number,　　　　10桁以上11桁以内の半角数値のみ保存可能なこと（良い例：09012345678　良くない例：090-1234-5678
     validates :buyer_id
   end
 
