6731f0b8e11aaeae2b9c53d8a2c2b222bb5a9c3f
akira-no <zzzzzgm4021@gmail.com>
2022-06-18 16:57:59 +0900

    テスト項目洗い出し

--- spec/factories/buyer_addresses.rb
+++ spec/factories/buyer_addresses.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :buyer_address do
+    token          {"tok_abcdefghijk00000000000000000"}
     post_code      {"111-1111"}
     city           {"市"}
     house_number   {"番地"}
--- spec/models/buyer_address_spec.rb
+++ spec/models/buyer_address_spec.rb
@@ -14,6 +14,49 @@ RSpec.describe BuyerAddress, type: :model do
       it '全項目正しく記述があれば購入できる' do
         expect(@buyer_address).to be_valid
       end
+      it '建物名は空白可' do
+      end
+    end
+
+    context '商品購入できないとき' do
+      it 'tokenがない' do
+      end
+      it '郵便番号がない' do
+      end
+      it '郵便番号に - がない' do
+      end
+      it '郵便番号の - が3桁 - 4桁以外に使用' do
+      end
+      it '郵便番号が全角数字入力' do
+      end
+      it '郵便番号の前半が3桁以外' do
+      end
+      it '郵便番号の後半が4桁以外' do
+      end
+      it '発送元入力がない' do
+      end
+      it '発送元選択が --- ' do
+      end
+      it '市区町村がない' do
+      end
+      it '番地がない' do
+      end
+      it '電話番号がない' do
+      end
+      it '電話番号が全角数字入力' do
+      end
+      it '電話番号に - を入力' do
+      end
+      it '電話番号に () を入力' do
+      end
+      it '電話番号が10桁未満' do
+      end
+      it '電話番号が11桁超過' do
+      end
+      it 'userが紐付いていない' do
+      end
+      it 'itemが紐づいていない' do
+      end
     end
   end
 end
